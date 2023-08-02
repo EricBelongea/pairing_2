8df02a3c8e2a02dad7d0d32d2a1bd525db828ef9
Eric Belongea <eric.belongea@gmail.com>
2023-08-02 12:46:43 -0600

    Add places and array methods

--- friends.rb
+++ friends.rb
@@ -3,3 +3,10 @@ arrays = ["Anthea","Eric","Aurora","Joseph","Justin"]
 puts arrays[2]
 puts arrays[-1]
 puts arrays[0]
+
+places = ["Rome","London","Stockholm","Tokyo","Hollywood"]
+
+puts places.pop
+puts places.shift
+puts places.unshift[2]
+
